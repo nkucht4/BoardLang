425be3f868eb605d3f3e9c1a824e775dd1b43728
Natalia <147085553+nxtalia@users.noreply.github.com>
2025-05-02 10:42:08 +0200

    Rerendering on file change

--- app/board_generator.py
+++ app/board_generator.py
@@ -1,28 +1,49 @@
 import pygame
-
+import os
+import time
+import json
 
 class BoardGenerator:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int, filename: str):
+        self.filename = filename
+        self.last_modified = os.path.getmtime(filename)
+
         pygame.init()
         self.screen = pygame.display.set_mode((x*20, y*20))
         self.clock = pygame.time.Clock()
         self.running = True
-    def render_tile(self, x: int, y: int, color: pygame.Color):
-        tile = pygame.Rect((x * 20, y * 20), (20, 20))
-        pygame.draw.rect(self.screen, color, tile)
 
     def run(self):
+        with open(self.filename, "rb") as f:
+            tilemap = json.load(f)
+        self.render_tilemap(tilemap)
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            board.render_tile(2, 2, pygame.Color("red"))
-            pygame.display.flip()
+            current_modified = os.path.getmtime(self.filename)
+            if current_modified != self.last_modified:
+                with open(self.filename, "rb") as f:
+                    tilemap = json.load(f)
+                self.render_tilemap(tilemap)
+                self.last_modified = current_modified
+
             self.clock.tick(10)
 
         pygame.quit()
 
+    def render_tilemap(self, tilemap):
+        for x, row in enumerate(tilemap):
+            for y, color in enumerate(row):
+                self.render_tile(x, y, pygame.Color(tuple(color)))
+        pygame.display.flip()
 
-board = BoardGenerator(5, 5)
+
+    def render_tile(self, x: int, y: int, color: pygame.Color):
+        tile = pygame.Rect((x * 20, y * 20), (20, 20))
+        pygame.draw.rect(self.screen, color, tile)
+
+
+board = BoardGenerator(2, 3, 'temp.json')
 board.run()
